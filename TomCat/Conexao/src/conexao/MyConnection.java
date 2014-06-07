ab3ae85f0d4f63e7c1afe2f599063c97ae87914b
Desenvolvimento <fabricioronchii@gmail.com>
2014-06-07 14:55:32 -0300

    Tom Cat

---
+++ TomCat/Conexao/src/conexao/MyConnection.java
@@ -0,0 +1,69 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/websocket";
+    private static final String USER = "root";
+    private static final String PASSWD = "root";
+    private static Connection con = null;
+    private static MyConnection conn;
+
+    static {
+        conn = new MyConnection();
+    }
+
+    public static MyConnection getInstance() {
+        return conn;
+    }
+
+    private MyConnection() {
+    }
+
+    public Connection getConnection() throws Exception {
+        if (con == null || con.isClosed()) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                con = DriverManager.getConnection(URL, USER, PASSWD);
+                con.setAutoCommit(true);
+            } catch (ClassNotFoundException e) {
+                System.err.println("Não consegui conectar em: " + URL);
+                throw e;
+            }
+        }
+
+        return con;
+    }
+
+    public static void closeConnection() throws Exception {
+        if (con != null || !con.isClosed()) {
+            try {
+                con.close();
+                con = null;
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static String showState() {
+        StringBuilder state = new StringBuilder("MyConnection state is... \n");
+        try {
+            Connection connection = getInstance().getConnection();
+            state.append("+ Connection instance Ok!\n");
+            if (connection.isClosed()) {
+                state.append("+ Connection is closed!\n");
+            } else {
+                state.append("+ Connection is open!\n");
+            }
+        } catch (Exception ex) {
+            state.append("MyConnction:showState() throws Exception ...\n");
+            state.append(ex.getMessage());
+        }
+
+        return state.toString();
+    }
+}
---
+++ TomCat/Conexao/src/conexao/UserDAO.java
@@ -0,0 +1,56 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserDAO extends AbstractDAO {
+
+    public final static String SQL_TO_FIND_ALL = "SELECT * FROM USER";
+
+    public ArrayList<User> findAll() throws Exception {
+        ArrayList<User> lst = new ArrayList<User>();
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            con = super.__getConnection();
+            stmt = con.prepareStatement(SQL_TO_FIND_ALL);
+            rs = super.__doDatabaseQuery(stmt);
+            while (rs.next()) {
+                lst.add(this.fillObject(rs));
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            super.__closeConnection(con, stmt, rs);
+        }
+
+        return lst;
+    }
+
+    public User getInstance(String nome, String senha, String email) {
+        User u = new User();
+        u.setNome(nome);
+        u.setSenha(senha);
+        u.setEmail(email);
+        return u;
+    }
+
+    public Object getNewInstance() {
+        return new User();
+    }
+
+    private User fillObject(ResultSet rs) throws SQLException {
+        User u = new User();
+        u.setId(rs.getInt("id"));
+        u.setNome(rs.getString("name"));
+        u.setSenha(rs.getString("email"));
+        u.setEmail(rs.getString("senha"));
+        return u;
+    }
+
+
+}
